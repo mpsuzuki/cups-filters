d1253eadd5f8d0d0c9b4d0561a47513623471596
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2017-12-25 10:19:27 +0900

    fix cpp/poppler-version.h path, pkg-config poppler-cpp --cflags returns a path including cpp subdirectory

--- filter/pdftoopvp/oprs/OPVPSplashXPath.cxx
+++ filter/pdftoopvp/oprs/OPVPSplashXPath.cxx
@@ -1,7 +1,7 @@
 #include <config.h>
 #include <stdio.h>
 #ifdef HAVE_CPP_POPPLER_VERSION_H
-#include "cpp/poppler-version.h"
+#include "poppler-version.h"
 #endif
 #include "splash/Splash.h"
 #include "splash/SplashMath.h"
